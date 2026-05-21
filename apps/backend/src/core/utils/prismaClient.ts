96e9641acb5cf21494e2d6532f21f4e651b1497c
rajwindersxxx <rajwindersxxx@gmail.com>
2026-05-21 22:28:06 +0530

    integate prisma

--- apps/backend/src/core/utils/prismaClient.ts
+++ apps/backend/src/core/utils/prismaClient.ts
@@ -6,28 +6,4 @@ const connectionString = env.databaseURL;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
-const clientCache = new Map<string, ReturnType<typeof createTenantClient>>();
-
-function createTenantClient(organizationId: string) {
-  return prisma.$extends({
-    query: {
-      $allModels: {
-        async $allOperations({ args, query }) {
-          const [, result] = await prisma.$transaction([
-            prisma.$executeRaw`SELECT set_config('app.current_organization', ${organizationId}, true);`,
-            query(args),
-          ]);
-          return result;
-        },
-      },
-    },
-  });
-}
-const getTenantClient = (organizationId: string) => {
-  if (!clientCache.has(organizationId)) {
-    // Validate UUID here if needed before caching
-    clientCache.set(organizationId, createTenantClient(organizationId));
-  }
-  return clientCache.get(organizationId)!;
-};
-export { prisma, getTenantClient };
+export { prisma };
